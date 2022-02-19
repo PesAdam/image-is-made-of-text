dd48ca0ac1640d43ec030dc0f4b8bb59c9459638
PesAdam <pesadam15@gmail.com>
2022-02-19 22:11:48 +0100

    seccond commit

--- js.js
+++ js.js
@@ -1,34 +1,33 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 let hauko;
 
 function preload(){
-    hauko = loadImage("gloria48.jpg");
+    hauko = loadImage("hauko.jpg");
 }
 
 function setup(){
-    createCanvas(400, 400);
-}
-
-function draw(){
-    background(220);
-    image(hauko, 0 , 0, width, height);
+    noCanvas();
+    //background(0);
+    //image(hauko, 0 , 0, width, height);
 
     let w = width / hauko.width;
     let h = height / hauko.height;
     hauko.loadPixels();
 
-    for(let i=0; i < hauko.width; i++){
-        for(let j=0; j < hauko.height; j++){
+    for(let j=0; j < hauko.height; j++){
+        let row = '';
+        for(let i=0; i < hauko.width; i++){
             const pixelIndex = (i+j*hauko.width) * 4;
             const r = hauko.pixels[pixelIndex + 0];
             const g = hauko.pixels[pixelIndex + 1];
             const b = hauko.pixels[pixelIndex + 2];
             const avg = (r+g+b) / 3;
-            noStroke();
-            fill(avg);
-            textSize(w);
-            text('G', i * w, j * h);
-
+            const len = density.length;
+            const charIndex = floor(map(avg, 0, 255, 0, len, 0));
+            const c = density.charAt(charIndex);
+            if(c == ' ') row += '&nbsp;'
+            else row += c;
         }
-    }        
+        createDiv(row);
+    }      
 }
